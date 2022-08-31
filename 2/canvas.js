cb93bc8045f616ea03fe333024a83aaaf66e36d7
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-08-31 18:26:03 +0200

    A few changes
    There is now an array that stores the random
    colors that can be used on the drawn objects.
    The objects are drawn at random coordinates
    and each time a resize happens the coordinates
    change.

--- 2/canvas.js
+++ 2/canvas.js
@@ -1,24 +1,49 @@
 const canvas = document.querySelector('canvas');
 
+const hslColors = [
+    'hsl(1, 100%, 65%, .5)',    // red
+    'hsl(45, 100%, 60%, .5)',   // yellow
+    'hsl(90, 100%, 40%, .5)',   // green
+    'hsl(180, 80%, 40%, .5)',   // blue
+    'hsl(270, 80%, 70%, .5)'    // purple
+];
+
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-// Used to draw the elements on the canvas
-const drawRect = () => {
-    const c = canvas.getContext('2d');
+const c = canvas.getContext('2d');
 
-    c.fillRect(0, 0, 100, 100);
-    c.fillRect(100, 100, 100, 100);
-    c.fillRect(200, 200, 100, 100);
-    c.fillRect(300, 300, 100, 100);
+// Used to draw the elements on the canvas
+const drawRect = (x, y) => {
+    c.fillRect(x, y, 100, 100);
 }
 
-drawRect();
+// Used to generate a random value from 0 to the passed value
+const getRandom = value => {
+    return Math.floor( Math.random() * value );
+}
+
+// Used to create a passed amount of rectangles with random colors
+const createRects = count => {
+    for(let i=0; i<count; i++)
+    {
+        const index = getRandom(hslColors.length);
+
+        c.fillStyle = hslColors[index];
+
+        const x = getRandom(window.innerWidth);
+        const y = getRandom(window.innerHeight);
+
+        drawRect(x, y);
+    }
+}
+
+createRects(5);
 
 // Used to redraw the elements on the canvas once the window changes size
 window.addEventListener('resize', () => {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 
-    drawRect();
+    createRects(5);
 })

f22f24d0b77a054d0bc4bf5b60251a811bf546f1
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-08-31 16:10:23 +0200

    Copied the folder 1 content into folder 2
    This is because the 2nd video uses the same set up
    used there.

---
+++ 2/canvas.js
@@ -0,0 +1,24 @@
+const canvas = document.querySelector('canvas');
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+// Used to draw the elements on the canvas
+const drawRect = () => {
+    const c = canvas.getContext('2d');
+
+    c.fillRect(0, 0, 100, 100);
+    c.fillRect(100, 100, 100, 100);
+    c.fillRect(200, 200, 100, 100);
+    c.fillRect(300, 300, 100, 100);
+}
+
+drawRect();
+
+// Used to redraw the elements on the canvas once the window changes size
+window.addEventListener('resize', () => {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    drawRect();
+})
